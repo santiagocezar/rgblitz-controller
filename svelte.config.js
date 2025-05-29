51751bdb40a57655a092e6cfe7071e289e085346
Santiago Cézar <santiagocezar2013@gmail.com>
2025-05-29 12:35:28 -0300

    base

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: "/rgblitz-controller"
+		}
+	}
 };
 
 export default config;
