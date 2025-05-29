99b1ad658e7d2683087a2c9637c0c7cfc4c4a539
Santiago Cézar <santiagocezar2013@gmail.com>
2025-05-29 12:24:34 -0300

    compare using hsl, use mqtt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
-    import { SvelteWebSocket } from "$lib/ws.svelte";
+    import { SvelteWebSocket } from "$lib/util.svelte";
+    import bingbong from "$lib/assets/bingbong.ogg";
     import Dial from "$lib/Dial.svelte";
 
     import Bulb from "~icons/hugeicons/bulb";
     import Tick from "~icons/hugeicons/tick-01";
     import Refresh from "~icons/hugeicons/refresh";
+    import mqtt from "mqtt";
+
+    let debug = $state(false);
 
     let light = $state(true);
     
     let puntos = $state(0);
 
     type Color = [r: number, g: number, b: number]
+    type HSL = { h: number, s: number, l: number }
 
     function randomize() {
         return [
@@ -19,9 +24,51 @@
             Math.floor((Math.random() ** 2) * 256),
         ] as Color
     }
+    
+
+    function rgbToHsl(r: number, g: number, b: number): HSL {
+        r /= 255, g /= 255, b /= 255;
+
+        let max = Math.max(r, g, b), min = Math.min(r, g, b);
+        let h = 0, s = 0, l = (max + min) / 2;
+
+        // not achromatic
+        if (max != min) {
+            const d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+            switch (max) {
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+
+            h /= 6;
+        }
+
+        return {
+            h: h * Math.PI * 2, 
+            s,
+            l
+        };
+    }
 
     let color1: Color = $state([128, 128, 128])
     let color2: Color = $state(randomize())
+
+    const hsl1 = $derived(rgbToHsl(...color1));
+    const hsl2 = $derived(rgbToHsl(...color2));
+
+    function hslToXY(color: HSL) {
+        const r = (1 - Math.abs(color.l - .5) * 2) * (color.s)
+        return {
+            x: r * Math.cos(color.h) * 50,
+            y: r * Math.sin(color.h) * 50,
+        }
+    }
+
+    const pos1 = $derived(hslToXY(hsl1));
+    const pos2 = $derived(hslToXY(hsl2));
 
     function deltaC([r1, g1, b1]: Color, [r2, g2, b2]: Color) {
         const rm = (r1 + r2) / 2
@@ -34,38 +81,42 @@
     }
 
     console.log("Test deltaC: " + deltaC([242, 212, 207], [242, 212, 207]))
-    const MAX_DELTA_C = deltaC([0, 0, 0], [255, 255, 255])
 
     function distanceRGB(colorA: Color, colorB: Color) {
-        return 100 - 100 * deltaC(colorA, colorB) / MAX_DELTA_C
+        return 100 - Math.sqrt((pos1.x - pos2.x) ** 2 + (pos1.y - pos2.y) ** 2)
     }
 
-    const ws = new SvelteWebSocket("http://192.168.132.24/ws")
+    const client = mqtt.connect("ws://broker.emqx.io:8083/mqtt", {
+        username: "santiagocezar",
+        password: "rgblitz0401",
 
-    $effect(() => {
-        const data = new Uint8Array([1, ...color1]).buffer
-        const conn = ws.connection
-        if (conn) {
-            conn.send(data)
-        }
     })
 
     $effect(() => {
-        const data = new Uint8Array([2, ...color2]).buffer
-        const conn = ws.connection
-        if (conn) {
-            conn.send(data)
-        }
+        const data = new Uint8Array([1, ...color1])
+        client.publish("santiagocezar/rgblitz", data)
     })
 
+    $effect(() => {
+        const data = new Uint8Array([2, ...color2])
+        client.publish("santiagocezar/rgblitz", data)
+    })
+    
     function reset() {
         puntos = 0
     }
 
+    let audio: HTMLAudioElement
     function check() {
-        if (distanceRGB(color1, color2) > 70) {
+        if (!audio) audio = new Audio(bingbong)
+        if (distanceRGB(color1, color2) > 80) {
             puntos ++
             color2 = randomize()
+            if (audio.paused) {
+                audio.play();
+            }else{
+                audio.currentTime = 0
+            }
         } else {
             reset()
         }
@@ -95,6 +146,12 @@
     })
 </script>
 
+<audio bind:this={audio} src={bingbong}></audio>
+
+<svelte:document onkeypress={(e) => {
+    e.code == "Backquote" && (debug = !debug)
+}} />
+
 <main data-light={light}>
     <div class="rgb">
         {#each color1 as comp, i}
@@ -104,6 +161,37 @@
             </div>
         {/each}
     </div>
+    {#if debug}
+        
+        <div class="debug">
+            {#snippet color(name: string, rgb: Color)}
+                {@const hsl = rgbToHsl(...rgb)}
+                <p>
+                    <span class="ref" style="background-color: rgb({rgb});"></span>
+                    {name}
+                </p>
+                <p>
+                    <strong>RGB:</strong> {rgb.map(v => v.toFixed(2)).join(", ")}
+                </p>
+                <p>
+                    <strong>H:</strong> {(hsl.h / Math.PI * 180).toFixed(2)}°
+                    <strong>S:</strong> {(hsl.s * 100).toFixed(2)}%
+                    <strong>L:</strong> {(hsl.l * 100).toFixed(2)}%
+                </p>
+            {/snippet}
+            {@render color("Color 1", color1)}
+            {@render color("Color 2", color2)}
+            <br>
+            <p>
+                <strong>Parecido:</strong> {distanceRGB(color1, color2).toFixed(2)}%
+            </p>
+            <svg viewBox="-50 -50 100 100">
+                <circle cx="0" cy="0" r="50" fill="#eee"></circle>
+                <circle cx={pos1.x} cy={pos1.y} r="5" fill="red"></circle>
+                <circle cx={pos2.x} cy={pos2.y} r="5" fill="blue"></circle>
+            </svg>
+        </div>
+    {/if}
     <header>
         <p class="score">{puntos}</p>
         
@@ -137,6 +225,32 @@ main {
     gap: 0 4vmin;
     transition: background-color .4s ease-out;
     padding: .5rem;
+}
+
+.debug {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background-color: #0002;
+    color: black;
+    padding: .5rem;
+    font-family: monospace;
+    line-height: 1.2;
+}
+
+.refs {
+    display: flex;
+}
+.ref {
+    display: inline-block;
+    vertical-align: middle;
+    width: 1.2em;
+    height: 1.2em;
+}
+.debug svg {
+    display: block;
+    width: 6rem;
+    height: 6rem;
 }
 
 main[data-light="true"] {
