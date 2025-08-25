469495ec291d154eeda10cb4c0bba0492972c3ce
Santiago Cézar <santiagocezar2013@gmail.com>
2025-08-25 16:44:15 -0300

    mostrar solo última notificación

--- src/lib/toasts/Toasts.svelte
+++ src/lib/toasts/Toasts.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import { toasts } from "./state.svelte";
 	import Toast from "./Toast.svelte";
+
+	const first = $derived(toasts.values().next().value);
 </script>
 
 <section
 	class="z-30 flex flex-col absolute inset-0 items-center justify-center pointer-events-none"
 >
-	{#each toasts.values() as toast (toast.id)}
+	{#if first}
+		<Toast {...first} />
+	{/if}
+	<!-- {#each toasts.values() as toast (toast.id)}
 		<Toast {...toast} />
-	{/each}
+	{/each} -->
 </section>
